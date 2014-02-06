987a4f752b79c14b02859aa666f5a1af02dac445
Evgeny Karataev <karataev.evgeny@gmail.com>
2014-02-06 19:23:12 +0000

    added logging4j2 for console only, refs #309

--- src/test/java/edu/pitt/sis/exp/colfusion/tests/ConfigManagerTest.java
+++ src/test/java/edu/pitt/sis/exp/colfusion/tests/ConfigManagerTest.java
@@ -1,12 +1,19 @@
 package edu.pitt.sis.exp.colfusion.tests;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import edu.pitt.sis.exp.colfusion.ConfigManager;
 import junit.framework.TestCase;
 
 public class ConfigManagerTest extends TestCase {
 	
+	Logger logger = LogManager.getLogger(ConfigManagerTest.class.getName());
+	
 	public void testGetParameterValueExisting() {
 		ConfigManager configManager = ConfigManager.getInstance();
+		
+		logger.warn("Testing log.");
 		
 		String testValue = configManager.getPropertyByName("test");
 		
