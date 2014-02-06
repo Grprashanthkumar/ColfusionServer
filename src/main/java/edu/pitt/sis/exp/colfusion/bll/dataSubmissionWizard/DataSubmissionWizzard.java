223d1c14957cbb54eca51ea08418a05da0d92417
Evgeny Karataev <karataev.evgeny@gmail.com>
2014-02-06 15:11:38 +0000

    starting to get closer to saving files to disk from ajax request, refs #306

--- src/main/java/edu/pitt/sis/exp/colfusion/bll/dataSubmissionWizard/DataSubmissionWizzard.java
+++ src/main/java/edu/pitt/sis/exp/colfusion/bll/dataSubmissionWizard/DataSubmissionWizzard.java
@@ -1,5 +1,32 @@
 package edu.pitt.sis.exp.colfusion.bll.dataSubmissionWizard;
 
-public class DataSubmissionWizzard {
+import java.io.InputStream;
+import java.util.List;
+import edu.pitt.sis.exp.colfusion.models.GeneralResponseModel;
 
+/**
+ * Handles all business logic for the data submisison wizard.
+ * 
+ * @author Evgeny
+ *
+ */
+public class DataSubmissionWizzard {
+	
+	/**
+	 * Stores the uploaded files into disk. Also it performs some actions depending on the file type. e.g. unzips archives.
+	 * 
+	 * @param sid story id for which the files are uploaded.
+	 * @param uploadTimestamp when the files were uploaded.
+	 * @param fileType the types of the file (most probably it is file extension, I will need to update this comment).
+	 * @param dbType if the uploaded file is database dump file, then this parameters says what database engine.
+	 * @param files the input streams of the files. 
+	 * @return
+	 */
+	public GeneralResponseModel StoreUploadedFiles(String sid, String uploadTimestamp, 
+    		String fileType, String dbType, List<InputStream> files) {
+				
+		
+		
+		return null;		
+	}
 }
--- src/main/java/edu/pitt/sis/exp/colfusion/controllers/WizardController.java
+++ src/main/java/edu/pitt/sis/exp/colfusion/controllers/WizardController.java
@@ -1,20 +1,29 @@
 package edu.pitt.sis.exp.colfusion.controllers;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.OPTIONS;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
 
+import org.glassfish.jersey.media.multipart.FormDataBodyPart;
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 import org.glassfish.jersey.media.multipart.FormDataParam;
 
+import edu.pitt.sis.exp.colfusion.bll.dataSubmissionWizard.DataSubmissionWizzard;
 import edu.pitt.sis.exp.colfusion.models.GeneralResponseModel;
+
 
 @Path("Wizard/")
 public class WizardController {
@@ -46,21 +55,26 @@ public class WizardController {
 	@Path("acceptFileFromWizard")
     @POST
     @Consumes(MediaType.MULTIPART_FORM_DATA)
- //   @Produces("application/json")
-    public String acceptFileFromWizard(@FormDataParam("sid") String sid, @FormDataParam("uploadTimestamp") String uploadTimestamp, 
+    @Produces("application/json")
+    public Response acceptFileFromWizard(
+    		@FormDataParam("sid") String sid, @FormDataParam("uploadTimestamp") String uploadTimestamp, 
     		@FormDataParam("fileType") String fileType,
-    		@FormDataParam("dbType") String dbType, 
-    		@FormDataParam("upload_file") InputStream file,
-    	    @FormDataParam("upload_file") FormDataContentDisposition fileDisposition ) {
+    		@FormDataParam("dbType") String dbType,
+    		@FormDataParam("upload_file[]") List<FormDataBodyPart> files
+    		 ) {
     	
-    	GeneralResponseModel grm = new GeneralResponseModel(); 
+		List<InputStream> inputStreams = new ArrayList<InputStream>();
+		for(FormDataBodyPart file : files){
+			inputStreams.add(file.getValueAs(InputStream.class));
+		}
+		
+    	GeneralResponseModel result; 
+    	DataSubmissionWizzard wizardBLL = new DataSubmissionWizzard();
+    	result = wizardBLL.StoreUploadedFiles(sid, uploadTimestamp, fileType, dbType, inputStreams);
     	
-    	grm.Status = "StatBLBL";
-    	grm.Message = "MsgBLBL";
-    	
-    	
-    	
-        return "haha";
+    	return Response.status(200).header("Access-Control-Allow-Origin", "http://localhost:8080")
+        	      .header("Access-Control-Allow-Methods", "POST, GET, PUT, UPDATE, OPTIONS")
+        	      .header("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With").entity(result).build();
     }
 }
 
