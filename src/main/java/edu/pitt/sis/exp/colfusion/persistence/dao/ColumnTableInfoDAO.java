3223e32c29f31be162b0f30175ca459169a5773e
Evgeny Karataev <karataev.evgeny@gmail.com>
2014-02-28 16:52:37 +0000

    added daos and mangers to save variables metadata, refs #338

---
+++ src/main/java/edu/pitt/sis/exp/colfusion/persistence/dao/ColumnTableInfoDAO.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package edu.pitt.sis.exp.colfusion.persistence.dao;
+
+import edu.pitt.sis.exp.colfusion.persistence.orm.ColfusionColumnTableInfo;
+
+/**
+ * @author Evgeny
+ *
+ */
+public interface ColumnTableInfoDAO extends GenericDAO<ColfusionColumnTableInfo, Integer> {
+
+}
