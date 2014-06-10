2212018a4c798fd8d3d11f7658a84caabe7b8703
Xiaolong Xu <mrxuxiaolong@gmail.com>
2014-06-10 19:45:11 +0000

    refs #449 doing refactoring

--- src/main/java/edu/pitt/sis/exp/colfusion/controllers/OpenRefineController.java
+++ src/main/java/edu/pitt/sis/exp/colfusion/controllers/OpenRefineController.java
@@ -3,8 +3,6 @@
  */
 package edu.pitt.sis.exp.colfusion.controllers;
 
-import java.sql.SQLException;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.OPTIONS;
@@ -17,16 +15,9 @@ import javax.ws.rs.core.Response;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import edu.pitt.sis.exp.colfusion.bll.CancelPreview;
 import edu.pitt.sis.exp.colfusion.bll.CreateProject;
-import edu.pitt.sis.exp.colfusion.bll.OpenRefineBL;
-import edu.pitt.sis.exp.colfusion.bll.OpenRefineCancelChanges;
 import edu.pitt.sis.exp.colfusion.bll.OpenRefineSaveChanges;
-import edu.pitt.sis.exp.colfusion.bll.SavePreview;
 import edu.pitt.sis.exp.colfusion.responseModels.GeneralResponseGen;
-import edu.pitt.sis.exp.colfusion.responseModels.GeneralResponseGenImpl;
-//import edu.pitt.sis.exp.colfusion.tests.openrefine.TestCreatePro;
-//import edu.pitt.sis.exp.colfusion.tests.openrefine.TestCreatePro2;
 
 /**
  * @author xxl
@@ -57,76 +48,6 @@ public class OpenRefineController  extends BaseController {
 	
 	
 	@OPTIONS
-    @Path("getProjectId/{sid}/{tableName}")
-    public Response getProjectId(@HeaderParam("Access-Control-Request-Headers") String requestH) {
-		return makeCORS(Response.ok()); //, requestH);
-    }
-	
-
-	@Path("getProjectId/{sid}/{tableName}")
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getProjectId(@PathParam("sid") int sid, @PathParam("tableName") String tableName) {
-    	
-		
-		OpenRefineBL openRefineBL = new OpenRefineBL();
-		
-//		JOptionPane.showMessageDialog(null, "This is a test msg", "Test Msg", JOptionPane.WARNING_MESSAGE); // Alex
-		// do some test in "testCreateProject"
-//		openRefineBL.testCreateProject();
-		
-//		TestCreatePro.testCreatePro();
-		
-		GeneralResponseGen<String> result = openRefineBL.getProjectID(sid, tableName);
-		
-    	
-    	return makeCORS(Response.status(200).entity(result)); //.build();
-    }
-	
-	@OPTIONS
-    @Path("cancelPreview/{sid}/{tableName}")
-    public Response cancelPreview(@HeaderParam("Access-Control-Request-Headers") String requestH) {
-		return makeCORS(Response.ok()); //, requestH);
-    }
-	
-
-	@Path("cancelPreview/{sid}/{tableName}")
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response cancelPreview(@PathParam("sid") int sid, @PathParam("tableName") String tableName) throws SQLException {
-    	
-		
-		CancelPreview cancelPreview = new CancelPreview();
-		
-		GeneralResponseGen<String> result = cancelPreview.cancelPreview(sid, tableName);
-		
-    	
-    	return makeCORS(Response.status(200).entity(result)); //.build();
-    }
-	
-	@OPTIONS
-    @Path("savePreview/{sid}/{tableName}")
-    public Response savePreview(@HeaderParam("Access-Control-Request-Headers") String requestH) {
-		return makeCORS(Response.ok()); //, requestH);
-    }
-	
-
-	@Path("savePreview/{sid}/{tableName}")
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response savePreview(@PathParam("sid") int sid, @PathParam("tableName") String tableName) throws SQLException {
-    	
-		
-		SavePreview savePreview = new SavePreview();
-		
-		GeneralResponseGen<String> result = savePreview.savePreview(sid, tableName);
-		
-    	
-    	return makeCORS(Response.status(200).entity(result)); //.build();
-    }
-	
-	
-	@OPTIONS
     @Path("saveChanges/{openRefinProjectId}")
     public Response saveChangesOptions(@HeaderParam("Access-Control-Request-Headers") String requestH) {
 		return makeCORS(Response.ok()); //, requestH);
@@ -140,24 +61,6 @@ public class OpenRefineController  extends BaseController {
 
 		OpenRefineSaveChanges openRefineSaveChanges = new OpenRefineSaveChanges();
 		GeneralResponseGen<String> result = openRefineSaveChanges.saveChanges(openRefinProjectId);
-		
-    	return makeCORS(Response.status(200).entity(result)); //.build();
-    }
-	
-	@OPTIONS
-    @Path("cancelChanges/{openRefinProjectId}")
-    public Response cancelChangesOptions(@HeaderParam("Access-Control-Request-Headers") String requestH) {
-		return makeCORS(Response.ok()); //, requestH);
-    }
-	
-
-	@Path("cancelChanges/{openRefinProjectId}")
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response cancelChanges(@PathParam("openRefinProjectId") String openRefinProjectId) throws ClassNotFoundException, Exception {
-
-		OpenRefineCancelChanges openRefineCancelChanges = new OpenRefineCancelChanges();
-		GeneralResponseGen<String> result = openRefineCancelChanges.cancelChanges(openRefinProjectId);
 		
     	return makeCORS(Response.status(200).entity(result)); //.build();
     }
