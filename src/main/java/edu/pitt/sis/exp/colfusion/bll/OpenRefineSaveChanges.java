d5784eeb471c6b3bf0206f237ac77b7ef18500f5
Xiaolong Xu <mrxuxiaolong@gmail.com>
2014-06-22 02:45:39 +0000

    refs #479 Got rid of all hard code by creating a new package "edu.pitt.sis.exp.colfusion.dao", put all settings in ColFusionOpenRefine.properties which is under src/main/resources/

--- src/main/java/edu/pitt/sis/exp/colfusion/bll/OpenRefineSaveChanges.java
+++ src/main/java/edu/pitt/sis/exp/colfusion/bll/OpenRefineSaveChanges.java
@@ -1,16 +1,16 @@
 package edu.pitt.sis.exp.colfusion.bll;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.Properties;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import edu.pitt.sis.exp.colfusion.dao.DatabaseHandler;
+import edu.pitt.sis.exp.colfusion.dao.MetadataDbHandler;
+import edu.pitt.sis.exp.colfusion.dao.TargetDatabaseHandlerFactory;
 import edu.pitt.sis.exp.colfusion.responseModels.GeneralResponseGen;
 import edu.pitt.sis.exp.colfusion.responseModels.GeneralResponseGenImpl;
 
@@ -25,22 +25,34 @@ final Logger logger = LogManager.getLogger(OpenRefineSaveChanges.class.getName()
 	 * @param tableName
 	 * @return
 	 * @throws SQLException 
+	 * @throws ClassNotFoundException 
+	 * @throws IOException 
 	 */
-	public GeneralResponseGen<String> saveChanges(final String projectId, final String colfusionUserId) throws SQLException {
+	public GeneralResponseGen<String> saveChanges(final String projectId, final String colfusionUserId) throws SQLException, ClassNotFoundException, IOException {
+		
+		Properties p = new Properties();
+        String fileName="/ColFusionOpenRefine.properties";
+        InputStream in = OpenRefineSaveChanges.class.getResourceAsStream(fileName);
+        p.load(in);  
+        in.close();
+        
+        int lockTime = Integer.valueOf(p.getProperty("lock_time"));
 		
 		GeneralResponseGen<String> result = new GeneralResponseGenImpl<>();
 		
 		result.setSuccessful(false);
 		
-		int sid = getSid(projectId);
-		String tableName = getTableName(projectId);
+		MetadataDbHandler metadataDbHandler = TargetDatabaseHandlerFactory.getMetadataDbHandler();
+        int sid = metadataDbHandler.getSid(projectId);
+        DatabaseHandler databaseHandler = TargetDatabaseHandlerFactory.getTargetDatabaseHandler(sid);
+        String tableName = metadataDbHandler.getTableNameByProjectId(projectId);
 		
 		String msg = "No change needs to be saved!";
-		if(!isTimeOutForCurrentUser(sid, tableName, Integer.valueOf(colfusionUserId))) {
-			if(tempTbExist(sid, tableName)) {
-				rmTb(sid, tableName);
-				createTb(sid, tableName);
-				rmTb(sid, "temp_" + tableName);
+		if(!metadataDbHandler.isTimeOutForCurrentUser(sid, tableName, Integer.valueOf(colfusionUserId), lockTime)) {
+			if(databaseHandler.tempTableExist(sid, tableName)) {
+				databaseHandler.removeTable(sid, tableName);
+				databaseHandler.createTable(sid, tableName);
+				databaseHandler.removeTable(sid, "temp_" + tableName);
 				msg = "Changes have been saved!";
 			}
 		} else {
@@ -48,234 +60,10 @@ final Logger logger = LogManager.getLogger(OpenRefineSaveChanges.class.getName()
 		}
 		
 		result.setMessage("OK");
-//		result.setPayload(msg + colfusionUserId);
 		result.setPayload(msg);
 		result.setSuccessful(true);
 			
 		
 		return result;
-	}
-	
-	public boolean isTimeOutForCurrentUser(final int sid, final String tableName, final int colfusionUserId) throws SQLException {
-		String driver = "com.mysql.jdbc.Driver";
-	    String dbURL = "jdbc:mysql://127.0.0.1:3306/colfusion"; // Connect
-	                                                                                // to
-	                                                                                // Server
-	                                                                                // &
-	                                                                                // DB连接服务器和数据库test
-	    String userName = "root"; // UserName 用户名
-	    String userPwd = ""; // Pwd 密码
-	    
-	    try {
-			Class.forName(driver);
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	    Connection conn = DriverManager.getConnection(dbURL, userName, userPwd);
-	    
-	    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
-        String currentTime = format.format(new Date());// new Date()为获取当前系统时间
-	    
-	    String query = "SELECT startChangeTime FROM colfusion_table_change_log WHERE endChangeTime is NULL and sid = " + sid + " AND tableName = '" + tableName + "' AND operatedUser = " + colfusionUserId;
-    	Statement ps = conn.createStatement();
-    	ResultSet rs = ps.executeQuery(query);
-    	
-    	String startTime = "";
-    	
-        while (rs.next()) {
-            startTime = rs.getString("startChangeTime");
-            return isTimeOutHelper(currentTime, startTime);
-        }
-        return true;
-	}
-	
-	public boolean isTimeOutHelper(final String currentTime, String startTime) {
-        if (!currentTime.split(" ")[0].equals(startTime.split(" ")[0])) {
-            return true;
-        }
-        startTime = startTime.substring(0, currentTime.length() - 1);
-        String[] arr1 = currentTime.split(" ")[1].split(":");
-        String[] arr2 = startTime.split(" ")[1].split(":");
-
-        int[] intArr1 = new int[arr1.length];
-        int[] intArr2 = new int[arr2.length];
-
-        for (int i = 0; i < arr1.length; i++) {
-            intArr1[i] = Integer.valueOf(arr1[i]);
-        }
-        for (int j = 0; j < arr2.length; j++) {
-            intArr2[j] = Integer.valueOf(arr2[j]);
-        }
-
-        if (intArr1[0] - intArr2[0] > 1) {
-            return true;
-        } else if (intArr1[0] - intArr2[0] == 1) {
-            if (intArr1[1] + 60 - intArr2[1] > 30) {
-                return true;
-            } else {
-                return false;
-            }
-        } else if (intArr1[0] - intArr2[0] == 0) {
-            if (intArr1[1] - intArr2[1] > 30) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-        return false;
-    }
-	
-	public void releaseTableLock(final int sid, final String tableName) throws SQLException {
-		String driver = "com.mysql.jdbc.Driver";
-	    String dbURL = "jdbc:mysql://127.0.0.1:3306/colfusion"; // Connect
-	                                                                                // to
-	                                                                                // Server
-	                                                                                // &
-	                                                                                // DB连接服务器和数据库test
-	    String userName = "root"; // UserName 用户名
-	    String userPwd = ""; // Pwd 密码
-	    
-	    try {
-			Class.forName(driver);
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	    Connection conn = DriverManager.getConnection(dbURL, userName, userPwd);
-	    
-	    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
-        String endEditTime = format.format(new Date());// new Date()为获取当前系统时间
-	    
-	    String query = "UPDATE colfusion_table_change_log SET endChangeTime = '" + endEditTime + "' WHERE endChangeTime IS NULL AND sid = " + sid + " AND tableName = '" + tableName + "'";
-    	Statement ps = conn.createStatement();
-    	ps.executeUpdate(query);
-	}
-	
-	public boolean tempTbExist(final int sid, final String tableName) throws SQLException {
-		String driver = "com.mysql.jdbc.Driver";
-	    String dbURL = "jdbc:mysql://127.0.0.1:3306/colfusion_filetodb_" + sid; // Connect
-	                                                                                // to
-	                                                                                // Server
-	                                                                                // &
-	                                                                                // DB连接服务器和数据库test
-	    String userName = "root"; // UserName 用户名
-	    String userPwd = ""; // Pwd 密码
-	    
-	    try {
-			Class.forName(driver);
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	    Connection conn = DriverManager.getConnection(dbURL, userName, userPwd);
-	    
-	    String query = "SHOW TABLES LIKE 'temp_" + tableName + "'";
-    	Statement ps = conn.createStatement();
-    	ResultSet result = ps.executeQuery(query);
-    	if(result.next()) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	
-	public void createTb(final int sid, final String tableName) throws SQLException {
-		String driver = "com.mysql.jdbc.Driver";
-	    String dbURL = "jdbc:mysql://127.0.0.1:3306/colfusion_filetodb_" + sid; // Connect
-	                                                                                // to
-	                                                                                // Server
-	                                                                                // &
-	                                                                                // DB连接服务器和数据库test
-	    String userName = "root"; // UserName 用户名
-	    String userPwd = ""; // Pwd 密码
-	    
-	    try {
-			Class.forName(driver);
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	    Connection conn = DriverManager.getConnection(dbURL, userName, userPwd);
-	    
-	    String query = "CREATE TABLE " + tableName + " SELECT * FROM temp_" + tableName;
-    	Statement ps = conn.createStatement();
-    	ps.executeUpdate(query);
-	}
-	
-	public void rmTb(final int sid, final String tableName) throws SQLException {
-		String driver = "com.mysql.jdbc.Driver";
-	    String dbURL = "jdbc:mysql://127.0.0.1:3306/colfusion_filetodb_" + sid; // Connect
-	                                                                                // to
-	                                                                                // Server
-	                                                                                // &
-	                                                                                // DB连接服务器和数据库test
-	    String userName = "root"; // UserName 用户名
-	    String userPwd = ""; // Pwd 密码
-	    
-	    try {
-			Class.forName(driver);
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	    Connection conn = DriverManager.getConnection(dbURL, userName, userPwd);
-	    
-	    String query = "DROP TABLE " + tableName;
-    	Statement ps = conn.createStatement();
-    	ps.execute(query);
-	}
-	
-	public int getSid(final String projectId) throws SQLException {
-		String driver = "com.mysql.jdbc.Driver";
-		String dbURL = "jdbc:mysql://127.0.0.1:3306/colfusion";
-		String userName = "root"; // UserName 用户名
-		String userPwd = ""; // Pwd 密码
-
-		Connection conn = null;
-		try {
-			Class.forName(driver);
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		conn = DriverManager.getConnection(dbURL, userName, userPwd);
-
-		String selectQuery = "select sid from colfusion_openrefine_project_map where projectId = '" + projectId + "'";
-		Statement ps = conn.createStatement();
-		ResultSet rs = ps.executeQuery(selectQuery);
-
-		String sid = "";
-		while (rs.next()) {
-			sid = rs.getString(1);
-		}
-		return Integer.valueOf(sid);
-	}
-	
-	public String getTableName(final String projectId) throws SQLException {
-		String driver = "com.mysql.jdbc.Driver";
-		String dbURL = "jdbc:mysql://127.0.0.1:3306/colfusion";
-		String userName = "root"; // UserName 用户名
-		String userPwd = ""; // Pwd 密码
-
-		Connection conn = null;
-		try {
-			Class.forName(driver);
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		conn = DriverManager.getConnection(dbURL, userName, userPwd);
-
-		String selectQuery = "select tableName from colfusion_openrefine_project_map where projectId = '" + projectId + "'";
-		Statement ps = conn.createStatement();
-		ResultSet rs = ps.executeQuery(selectQuery);
-
-		String tableName = "";
-		while (rs.next()) {
-			tableName = rs.getString(1);
-		}
-		return tableName;
 	}
 }
---
+++ src/main/java/edu/pitt/sis/exp/colfusion/dao/TargetDatabaseHandlerFactory.java
@@ -0,0 +1,88 @@
+/*
+
+Copyright 2014, xxl
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+ * Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above
+copyright notice, this list of conditions and the following disclaimer
+in the documentation and/or other materials provided with the
+distribution.
+ * Neither the name of Google Inc. nor the names of its
+contributors may be used to endorse or promote products derived from
+this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,           
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY           
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ */
+package edu.pitt.sis.exp.colfusion.dao;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+/**
+ * @author xxl
+ *
+ */
+public class TargetDatabaseHandlerFactory {
+    private static MetadataDbHandler metadataDbHandler;
+
+    final static Logger logger = LogManager.getLogger(TargetDatabaseHandlerFactory.class.getName());
+    
+    static {
+    	Properties p = new Properties();
+        String fileName="/ColFusionOpenRefine.properties";
+        InputStream in = TargetDatabaseHandlerFactory.class.getResourceAsStream(fileName);
+        try {
+			p.load(in);
+			in.close();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}  
+        String host = p.getProperty("mysql_host");
+        int port = Integer.valueOf(p.getProperty("mysql_port"));
+        String user = p.getProperty("mysql_user");
+        String password = p.getProperty("mysql_password");
+        String database = p.getProperty("mysql_database");
+    	
+        //TODO:　Read host,port, etc. from config file and/or system properties
+        DatabaseConnectionInfo connectioInfo = new DatabaseConnectionInfo(host, port, user, password, database);
+        try {
+            metadataDbHandler = new MetadataDbHandler(new MySQLDatabaseHandler(connectioInfo));
+        } catch (ClassNotFoundException e) {
+           logger.error("Couldn't intinialize meatdata db handler", e);
+        }
+    }
+    
+    public static DatabaseHandler getTargetDatabaseHandler(int sid) throws SQLException, ClassNotFoundException {
+        DatabaseConnectionInfo connectioInfo = metadataDbHandler.getTargetDbConnectionInfo(sid);
+        
+       //TODO: this whole thing should be in a separate project and be shared with ColFusion
+        return new MySQLDatabaseHandler(connectioInfo);
+    }
+    
+    public static MetadataDbHandler getMetadataDbHandler() {
+        return metadataDbHandler;
+    }
+}
