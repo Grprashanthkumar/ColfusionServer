2fde81c27f854af0aee2a8cbf29ba5ba1b6d0259
Shadi Khateeb <shadikhateeb@live.com>
2014-10-04 04:29:59 +0000

    Update the databaseHandlers package the Path for this package is ColFusion Server\ColFusion Server\ColFusionServer\src\main\java\edu\pitt\sis\exp\colfusion\persistence\databaseHandlers refs #566

---
+++ ColFusionServer/src/main/java/edu/pitt/sis/exp/colfusion/persistence/databaseHandlers/DatabaseConnectionInfo.java
@@ -0,0 +1,38 @@
+package edu.pitt.sis.exp.colfusion.persistence.databaseHandlers;
+
+public class DatabaseConnectionInfo {
+    private String host;
+    private int port;
+    private String user;
+    private String password;
+    private String database;
+    
+    public DatabaseConnectionInfo(String host, int port, String user, String password,
+            String database){
+        this.host = host;
+        this.port = port;
+        this.user = user;
+        this.password = password;
+        this.database = database;
+    }
+    
+    public String getHost() {
+        return host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getDatabase() {
+        return database;
+    }
+}
