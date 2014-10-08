d9733ccd35b1da421ff671b62063dad6dbc5b9b8
Weichuan Hong <weichuan0903@gmail.com>
2014-10-08 18:15:25 +0000

    refs #554

---
+++ ColFusionServer/src/main/java/edu/pitt/sis/exp/colfusion/responseModels/StoryStatusResponseModel.java
@@ -0,0 +1,29 @@
+package edu.pitt.sis.exp.colfusion.responseModels;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.pitt.sis.exp.colfusion.dal.orm.ColfusionExecuteinfo;
+
+
+/**
+ * @author Weichuan Hong
+ *
+ */
+public class StoryStatusResponseModel extends GeneralResponseImpl{
+	private List<ColfusionExecuteinfo> payload = new ArrayList<ColfusionExecuteinfo>();
+
+	/**
+	 * @return the payload
+	 */
+	public List<ColfusionExecuteinfo> getPayload() {
+		return payload;
+	}
+
+	/**
+	 * @param payload the payload to set
+	 */
+	public void setPayload(List<ColfusionExecuteinfo> payload) {
+		this.payload = payload;
+	}
+}
