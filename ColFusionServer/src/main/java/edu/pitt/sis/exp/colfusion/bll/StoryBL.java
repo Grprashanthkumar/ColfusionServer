7e95c677238cba698f1fe97fd291e067be582694
Weichuan Hong <weichuan0903@gmail.com>
2014-10-22 16:11:47 +0000

    refs #592

--- ColFusionServer/src/main/java/edu/pitt/sis/exp/colfusion/bll/StoryBL.java
+++ ColFusionServer/src/main/java/edu/pitt/sis/exp/colfusion/bll/StoryBL.java
@@ -141,7 +141,10 @@ public class StoryBL {
 			StoryMetadataViewModel storyMetadata = new StoryMetadataViewModel();
 			storyMetadata.setSid(sid);
 			storyMetadata.setSourceType(storyInfo.getSourceType());
-			storyMetadata.setLicenseId(storyInfo.getColfusionLicense().getLicenseId());
+			
+			if (storyInfo.getColfusionLicense() != null) {
+				storyMetadata.setLicenseId(storyInfo.getColfusionLicense().getLicenseId());
+			}
 			
 			
 			//Getting user's role in the story and put in the view model with role details: role name, description and role id.
