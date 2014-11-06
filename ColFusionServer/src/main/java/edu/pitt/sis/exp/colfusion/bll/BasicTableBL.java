53482c7b9b65d3c25d034b8bc5e40d94f9154d01
Weichuan Hong <weichuan0903@gmail.com>
2014-11-06 04:38:25 +0000

    refs #615 annotation delete

--- ColFusionServer/src/main/java/edu/pitt/sis/exp/colfusion/bll/BasicTableBL.java
+++ ColFusionServer/src/main/java/edu/pitt/sis/exp/colfusion/bll/BasicTableBL.java
@@ -45,65 +45,7 @@ public class BasicTableBL {
 			result.isSuccessful=false;
 			result.message = "Get DNameInfo failed";
 		}
-		return result;
-		
-//		BasicTableResponseModel result=new BasicTableResponseModel();
-//		
-//        String driver = "com.mysql.jdbc.Driver";
-//
-//        String url = "jdbc:mysql://127.0.0.1:3306/colfusion";
-//        String user = "dataverse"; 
-//        String password = "dataverse";
-//
-//        try { 
-//        	
-//        	List<BasicTableInfoViewModel> tableInfoFromDB= new ArrayList<BasicTableInfoViewModel>();
-//        	
-//	         Class.forName(driver);
-//	         Connection conn = DriverManager.getConnection(url, user, password);
-//	
-//	         if(!conn.isClosed()) {
-////	          System.out.println("Succeeded connecting to the Database!");
-//	         }
-//	         Statement statement = conn.createStatement();
-//	         String sql = "SELECT * FROM colfusion_columnTableInfo natural join colfusion_dnameinfo where sid = "+sid;
-//	
-//	         ResultSet rs = statement.executeQuery(sql);
-//         
-//
-//	
-//	         while(rs.next()) {
-//	        	 BasicTableInfoViewModel tableInfo = new BasicTableInfoViewModel();
-//	        	 tableInfo.setCid(rs.getString("cid"));
-//	        	 tableInfo.setDname_chosen(rs.getString("dname_chosen"));
-//	        	 tableInfo.setDname_original_name(rs.getString("dname_original_name"));
-//	        	 tableInfo.setDname_value_description(rs.getString("dname_value_description"));
-//	        	 tableInfo.setDname_value_type(rs.getString("dname_value_type"));
-//	        	 tableInfo.setDname_value_unit(rs.getString("dname_value_unit"));
-//	        	 tableInfoFromDB.add(tableInfo);
-//	        	 
-//	        	 System.out.println(rs.getString("cid"));
-//	          	 System.out.println(rs.getString("dname_chosen"));
-//         }
-//	         
-////	         result=tableInfoFromDB;
-//	         result.setPayload(tableInfoFromDB);
-//	         result.isSuccessful=true;
-//	         result.message="OK";
-//	         System.out.println(result.getPayload().get(0).getCid());
-//	         rs.close();
-//	         conn.close();
-//        } catch(ClassNotFoundException e) {
-//	         System.out.println("Sorry,can`t find the Driver!"); 
-//	         e.printStackTrace();  
-//        } catch(SQLException e) {
-//        	 e.printStackTrace();
-//        } catch(Exception e) {
-//        	 e.printStackTrace();
-//        } 
-//
-//
-//		return result;
+		return result
 		
 	}
 	
