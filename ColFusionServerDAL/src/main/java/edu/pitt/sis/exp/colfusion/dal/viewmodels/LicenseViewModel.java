a75dd2e72ba2c7e5e5fbe53e6bd014eacff8e5f6
Evgeny Karataev <karataev.evgeny@gmail.com>
2014-11-06 21:19:43 +0000

    refs #614 refacted license

--- ColFusionServerDAL/src/main/java/edu/pitt/sis/exp/colfusion/dal/viewmodels/LicenseViewModel.java
+++ ColFusionServerDAL/src/main/java/edu/pitt/sis/exp/colfusion/dal/viewmodels/LicenseViewModel.java
@@ -14,23 +14,49 @@ public class LicenseViewModel {
 	@Expose private int licenseId;
 	@Expose private String licenseName;
 	@Expose private String licenseURL;
+	@Expose private String licenseDescription;
+	
+	public LicenseViewModel() {
+		
+	}
+	
+	public LicenseViewModel(final Integer licenseId, final String licenseName,
+			final String licenseUrl, final String licenseDescription) {
+		setLicenseId(licenseId);
+		setLicenseName(licenseName);
+		setLicenseURL(licenseUrl);
+		setLicenseDescription(licenseDescription);
+	}
+
 	public int getLicenseId() {
 		return licenseId;
 	}
-	public void setLicenseId(int licenseId) {
+	
+	public void setLicenseId(final int licenseId) {
 		this.licenseId = licenseId;
 	}
+	
 	public String getLicenseName() {
 		return licenseName;
 	}
-	public void setLicenseName(String licenseName) {
+	
+	public void setLicenseName(final String licenseName) {
 		this.licenseName = licenseName;
 	}
+	
 	public String getLicenseURL() {
 		return licenseURL;
 	}
-	public void setLicenseURL(String licenseURL) {
+	
+	public void setLicenseURL(final String licenseURL) {
 		this.licenseURL = licenseURL;
 	}
 
+	public String getLicenseDescription() {
+		return licenseDescription;
+	}
+	
+	public void setLicenseDescription(final String licenseDescription) {
+		this.licenseDescription = licenseDescription;
+	}
 }
