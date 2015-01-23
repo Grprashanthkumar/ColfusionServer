22ab1f979bd85d635126adbfe339bcb900731913
lxtlouise <lxtlouise2014@gmail.com>
2015-01-23 03:39:54 +0000

    refs #672 Add new test on the new function named findByName

--- ColFusionServer/src/test/java/edu/pitt/sis/exp/colfusion/tests/bll/VisualizationTest.java
+++ ColFusionServer/src/test/java/edu/pitt/sis/exp/colfusion/tests/bll/VisualizationTest.java
@@ -2,6 +2,9 @@ package edu.pitt.sis.exp.colfusion.tests.bll;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import edu.pitt.sis.exp.colfusion.bll.VisualizationBL;
@@ -12,6 +15,7 @@ import edu.pitt.sis.exp.colfusion.dal.orm.ColfusionCanvases;
 import edu.pitt.sis.exp.colfusion.dal.orm.ColfusionUsers;
 
 public class VisualizationTest extends DatabaseUnitTestBase{
+	
 	@Test
 	public void testCreateNewCanvas() throws Exception {
 		
@@ -28,5 +32,20 @@ public class VisualizationTest extends DatabaseUnitTestBase{
 		ColfusionCanvases actualCanvases = canvasMng.findByID(canvases.getVid());
 		
 		assertEquals("Wrong canvas name", canvases.getName(), actualCanvases.getName());
+	}
+	
+	@Test
+	public void testFindByName() throws Exception {
+		String canvasName = "testCanvas";
+		
+		VisualizationBL visualizationBL = new VisualizationBL();
+		List<ColfusionCanvases> canvasesList = visualizationBL.findByName(canvasName);
+		
+		assertNotNull(canvasesList);
+		
+		CanvasesManager canvasMng = new CanvasesManagerImpl();
+		List<ColfusionCanvases> actualCanvasesList = canvasMng.findByName(canvasName);
+		
+		assertEquals("Wrong canvas list", canvasesList, actualCanvasesList);
 	}
 }
